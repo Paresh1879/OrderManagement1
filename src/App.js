fd129d974214ae852170b8e1a654c298ec5e8548
Paresh <paresh@Pareshs-MacBook-Air.local>
2022-09-25 10:53:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import {BrowserRouter, Switch , Route} from 'react-router-dom';
+
+//import Link from '@material-ui/core/Link';
+import Order from './Order';
+import Home from './Home';
+
+
+
+function App() {
+
+  
+  return (
+    <>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/orders" component={Order} />
+      </Switch>
+    </BrowserRouter>
+
+    
+    </>
+  );
+}
+
+export default App;
